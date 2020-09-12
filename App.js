2131b7a1a3a5a4d10c6586063a899e42058f452e
ambujdubey <ambujdubey89@gmail.com>
2020-09-12 13:38:34 +0530

    update list using react hook

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [ inputText, setInputText ] = useState('');
+  const [ allGoalsList, setAllGoalsList ] = useState([]);
+  const setInputTextHandler = (test) => {
+    setInputText(test);
+  }
+  
+  const setAddNewGoalList = () => {
+    console.log('value : ',inputText);
+    setAllGoalsList([...allGoalsList, inputText]);
+    console.log('allGoalsList : ',allGoalsList);
+  }
   return (
     <View style={{padding:50}}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-      <TextInput 
-        placeholder='Cource Goal'
-        style={{width: 200, borderColor: 'black', borderWidth : 1, padding: 10 }}
-      />
-      <Button title='Add'/>
+        <TextInput 
+          placeholder='Course Goal'
+          style={{width: 200, borderColor: 'black', borderWidth : 1, padding: 10 }}
+          onChangeText = {setInputTextHandler}
+          value = {inputText}
+        />
+        <Button title='Add' onPress={setAddNewGoalList}/>
+      </View>
+      <View>
+        {allGoalsList.map( (goal) => <Text key={ goal }>{ goal }</Text>)}        
       </View>
     </View>
   );
