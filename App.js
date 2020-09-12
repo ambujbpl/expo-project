e8bb3d01b9f4b9ce45f89f4c9dcf7e3e4bf7d376
ambujdubey <ambujdubey89@gmail.com>
2020-09-12 19:24:56 +0530

    cancel button in modal cear input field on submit and add button css

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React , { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import { 
+  StyleSheet,
+  View,
+  Button,
+  FlatList
+} from 'react-native';
 import ItemList from './components/itemList';
 import ItemInput from './components/itemInput';
 
 export default function App() {
  
   const [ allGoalsList, setAllGoalsList ] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
   const setAddNewGoalList = (inputText) => {
     setAllGoalsList([...allGoalsList, { key: Math.random().toString(), value: inputText}]);
-  }
+    cancelAddNewGoal();
+  };
   const deleteItemOnGoalList = (id) => {
     setAllGoalsList(currentAllGoalsList => {
       return currentAllGoalsList.filter((goal) => goal.key != id)
     });
-  }
+  };
+  const cancelAddNewGoal = () => {
+    setModalVisible(false);
+  };
 
   return (
     <View style={styles.mainView}>
-      <ItemInput onAddGoal={setAddNewGoalList}/>
+      <Button title='Add New Goal' onPress={() => setModalVisible(true)}/>
+      <ItemInput 
+        onAddGoal={setAddNewGoalList} 
+        modalVisibleCheck={modalVisible}
+        cancelAddNewGoalFun={cancelAddNewGoal} 
+      />
       <FlatList 
         data={allGoalsList} 
         renderItem={ itemData => 
--- components/itemInput.js
+++ components/itemInput.js
@@ -1,31 +1,75 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { 
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Alert,
+  Modal,
+} from 'react-native';
 
 const ItemInput = props => {  
   const [ inputText, setInputText ] = useState('');
   const setInputTextHandler = (test) => {
     setInputText(test);
   }
+  const validatInputValueBeforeAddIngoalList = () => {
+    if(inputText != '' && inputText.length > 2) {
+      props.onAddGoal(inputText);
+      setInputText('');
+    } else {
+      Alert.alert('Name field should not empty and minimum length will be 3');
+    }
+  };
   return (
-    <View style={styles.rowView}>
-      <TextInput 
-        placeholder='Course Goal'
-        style={{width: 200, borderColor: 'black', borderWidth : 1, padding: 10 }}
-        onChangeText = {setInputTextHandler}
-        value = {inputText}
-      />
-      {/* <Button title='Add' onPress={() => props.onAddGoal(inputText)}/> */}
-      <Button title='Add' onPress={props.onAddGoal.bind(this,inputText)}/>
-    </View>
+    <Modal
+      animationType="slide"
+      transparent={false}
+      visible={props.modalVisibleCheck}
+      onRequestClose={() => {
+        Alert.alert("Modal has been closed.");
+      }}>
+      <View style={styles.rowView}>
+        <TextInput 
+          placeholder='Course Goal'
+          style={styles.inputTextCss}
+          onChangeText = {setInputTextHandler}
+          value = {inputText}/>
+          {/* <Button title='Add' onPress={() => props.onAddGoal(inputText)}/> */}
+        <View style={styles.buttonContainer}>
+          <View style={styles.buttonCss}>
+            <Button title="Cancel" color="red" onPress={props.cancelAddNewGoalFun} />
+          </View> 
+          <View style={styles.buttonCss}>
+            <Button title='Add' color="green" onPress={validatInputValueBeforeAddIngoalList}/>
+          </View>          
+        </View>
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
   rowView: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center'
   },
+  inputTextCss: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth : 1,
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '80%'
+  },
+  buttonCss: {
+    width: '40%'
+  }
 });
 
 export default ItemInput;
