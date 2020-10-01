cc361dc77f4861d2041f210c4324d698b50a9794
ambujdubey <ambujdubey89@gmail.com>
2020-10-01 06:17:02 +0530

    apply same properties to all stack navigation screen headers

--- app/navigation/MealsNavigator.js
+++ app/navigation/MealsNavigator.js
@@ -1,16 +1,32 @@
+import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-
+import { createStackNavigator } from 'react-navigation-stack'
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Colors from '../constants/Colors';
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMealsScreen
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen
+    },
+    CategoryMeals: {
+      screen: CategoryMealsScreen
+    },
+    MealDetail: MealDetailScreen
   },
-  MealDetail: MealDetailScreen
-});
+  {
+    // initialRouteName: 'Categories',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+      },
+      headerTintColor:
+        Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+      headerTitle: 'A Screen'
+    }
+  }
+);
 
 export default createAppContainer(MealsNavigator);
--- app/screens/CategoryMealsScreen.js
+++ app/screens/CategoryMealsScreen.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Platform } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors';
 
 const CategoryMealScreen = props => {
   const catId = props.navigation.getParam('categoryId');
@@ -37,11 +36,7 @@ CategoryMealScreen.navigationOptions = navigationData => {
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
   return {
-    headerTitle: selectedCategory.title,
-    headerStyle: {
-      backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-    },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    headerTitle: selectedCategory.title
   };
 };
 
