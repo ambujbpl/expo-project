a428e375680fba2e526ac116076b6a0320ea0d16
ambujdubey <ambujdubey89@gmail.com>
2020-10-18 09:55:59 +0530

    add addNew and edit Product functoinality in admin area

--- app/navigation/ShopNavigator.js
+++ app/navigation/ShopNavigator.js
@@ -10,6 +10,7 @@ import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
+import EditProductScreen from '../screens/user/EditProductScreen';
 import Colors from '../constants/Colors';
 
 const defaultNavOptions = {
@@ -65,7 +66,8 @@ const OrdersNavigator = createStackNavigator(
 
 const AdminNavigator = createStackNavigator(
     {
-      UserProducts: UserProductsScreen
+      UserProducts: UserProductsScreen,
+      EditProduct: EditProductScreen
     },
     {
       navigationOptions: {
