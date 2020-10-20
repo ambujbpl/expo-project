85ab796d4093b8d483665d6585386ada9905613b
Ambuj Dubey <ambujdubey89@gmail.com>
2020-10-20 18:33:32 +0530

    Update firebase.js

--- projects/6-Shop-App/store/actions/firebase.js
+++ projects/6-Shop-App/store/actions/firebase.js
@@ -1,4 +1,4 @@
 module.exports = {
   url: `https://app-store-project.firebaseio.com`,
-  web_api_key: `AIzaSyBlZG5g8l8la4ArLU0P-PUtT3ec-Vj22Wo`
+  web_api_key: `Asdkndscdscsadasdsad-PUtT3ec-Vj22Wo`
 }
