4589c35bdc13ce19bc4583ce77bf14b82f8cb591
ambujdubey <ambujdubey89@gmail.com>
2020-10-20 06:43:33 +0530

    add loader during auth and add error handling as firebase response

--- app/store/actions/auth.js
+++ app/store/actions/auth.js
@@ -19,8 +19,14 @@ export const signup = (email, password) => {
     );
 
     if (!response.ok) {
-      throw new Error('Something went wrong!');
-    }
+        const errorResData = await response.json();
+        const errorId = errorResData.error.message;
+        let message = 'Something went wrong!';
+        if (errorId === 'EMAIL_EXISTS') {
+          message = 'This email exists already!';
+        }
+        throw new Error(message);
+      }
 
     const resData = await response.json();
     console.log(resData);
@@ -29,28 +35,36 @@ export const signup = (email, password) => {
 };
 
 export const login = (email, password) => {
-    return async dispatch => {
-      const response = await fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBlZG5g8l8la4ArLU0P-PUtT3ec-Vj22Wo',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-            returnSecureToken: true
-          })
-        }
-      );
-  
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBlZG5g8l8la4ArLU0P-PUtT3ec-Vj22Wo',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
       }
-  
-      const resData = await response.json();
-      console.log(resData);
-      dispatch({ type: LOGIN });
-    };
+    );
+
+    if (!response.ok) {
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = 'Something went wrong!';
+      if (errorId === 'EMAIL_NOT_FOUND') {
+        message = 'This email could not be found!';
+      } else if (errorId === 'INVALID_PASSWORD') {
+        message = 'This password is not valid!';
+      }
+      throw new Error(message);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: LOGIN });
   };
+};
