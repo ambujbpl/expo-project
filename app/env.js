3d2c31945adeecf1269c08a9a6e4dad1109d6985
ambujdubey <ambujdubey89@gmail.com>
2020-11-18 19:28:05 +0530

    update malta cordinates to view Malta map on screen

--- app/env.js
+++ app/env.js
@@ -1,5 +1,9 @@
 const vars = {
-    googleApiKey: 'AIzaSyD-ZPcMh4YKvZJSo-H2C7ESf2PEDJWlcEc'
+    googleApiKey: 'AIzaSyD-ZPcMh4YKvZJSo-H2C7ESf2PEDJWlcEc',
+    latitude: 35.9375,
+    longitude: 14.3754,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.421
 };
 
 export default vars;
--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
+import ENV from '../env';
 
 const MapScreen = props => {
   const mapRegion = {
-    latitude: 37.78,
-    longitude: -122.43,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
+    latitude: ENV.latitude,
+    longitude: ENV.longitude,
+    latitudeDelta: ENV.latitudeDelta,
+    longitudeDelta: ENV.longitudeDelta
   };
   return <MapView style={styles.map} region={mapRegion} />;
 };
