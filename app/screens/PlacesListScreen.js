56735be905b89af2932d9887384c369ef10eacd4
ambujdubey <ambujdubey89@gmail.com>
2020-11-18 06:01:18 +0530

    add custom header and stack navigation

--- app/screens/PlacesListScreen.js
+++ app/screens/PlacesListScreen.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
 
 const PlacesListScreen = props => {
   return (
@@ -9,8 +12,21 @@ const PlacesListScreen = props => {
   );
 };
 
-PlacesListScreen.navigationOptions = {
-    headerTitle: 'All Places'
+PlacesListScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'All Places',
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add Place"
+          iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+          onPress={() => {
+              navData.navigation.navigate('NewPlace');
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({});
