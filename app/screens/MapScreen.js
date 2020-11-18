aacbcc055b0cac2ec696ba3ad8f26548b24449c0
ambujdubey <ambujdubey89@gmail.com>
2020-11-18 19:39:10 +0530

    add marker onclick event

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+
 import ENV from '../env';
 
 const MapScreen = props => {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const mapRegion = {
     latitude: ENV.latitude,
     longitude: ENV.longitude,
     latitudeDelta: ENV.latitudeDelta,
     longitudeDelta: ENV.longitudeDelta
   };
-  return <MapView style={styles.map} region={mapRegion} />;
+
+  const selectLocationHandler = event => {
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude
+    });
+  };
+
+  let markerCoordinates;
+
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.lat,
+      longitude: selectedLocation.lng
+    };
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      region={mapRegion}
+      onPress={selectLocationHandler}
+    >
+      {markerCoordinates && (
+        <Marker title="Picked Location" coordinate={markerCoordinates} />
+      )}
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
