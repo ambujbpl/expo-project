2167930a618a643aa0d115737ff6be9e8c7f019d
ambujdubey <ambujdubey89@gmail.com>
2020-11-20 16:28:35 +0530

    add react navigation version 5 dependency and update map project inside projects folder

---
+++ projects/7-Map-App/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
+import PlacesNavigator from './app/navigation/PlacesNavigator';
+import placesReducer from './app/store/places-reducer';
+import { init } from './app/helpers/db';
+
+init()
+  .then(() => {
+    console.log('Initialized database');
+  })
+  .catch(err => {
+    console.log('Initializing db failed.');
+    console.log(err);
+  });
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  );
+}
---
+++ projects/7-Map-App/app/env.js
@@ -0,0 +1,9 @@
+const vars = {
+    googleApiKey: 'AIzaSyBlZG5g8l8la4ArLU0P-PUtT3ec-Vj22Wo',
+    latitude: 35.9375,
+    longitude: 14.3754,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.421
+};
+
+export default vars;
