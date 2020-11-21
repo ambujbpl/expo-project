669b22bc02a60155a43dc9d7ef6a96f968723314
ambujdubey <ambujdubey89@gmail.com>
2020-11-22 04:46:29 +0530

    add push notification

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Button, View } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
@@ -13,6 +13,8 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+  const [pushToken, setPushToken] = useState();
+
   useEffect(() => {
     Permissions.getAsync(Permissions.NOTIFICATIONS)
       .then((statusObj) => {
@@ -23,8 +25,19 @@ export default function App() {
       })
       .then((statusObj) => {
         if (statusObj.status !== 'granted') {
-          return;
+          throw new Error('Permission not granted!');
         }
+      })
+      .then(() => {
+        return Notifications.getExpoPushTokenAsync();
+      })
+      .then((response) => {
+        const token = response.data;
+        setPushToken(token);
+      })
+      .catch((err) => {
+        console.log(err);
+        return null;
       });
   }, []);
 
@@ -48,15 +61,29 @@ export default function App() {
   }, []);
 
   const triggerNotificationHandler = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: 'My first local notification',
-        body: 'This is the first local notification we are sending!',
-        data: { mySpecialData: 'Some text' },
+    // Notifications.scheduleNotificationAsync({
+    //   content: {
+    //     title: 'My first local notification',
+    //     body: 'This is the first local notification we are sending!',
+    //     data: { mySpecialData: 'Some text' },
+    //   },
+    //   trigger: {
+    //     seconds: 10,
+    //   },
+    // });
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-Encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
       },
-      trigger: {
-        seconds: 10,
-      },
+      body: JSON.stringify({
+        to: pushToken,
+        data: { extraData: 'Some data' },
+        title: 'Sent via the app',
+        body: 'This push notification was sent via the app!',
+      }),
     });
   };
 
