0fb6da46e068c8f204055836d4a4bf27d7be0506
ambujdubey <ambujdubey89@gmail.com>
2020-11-21 16:17:28 +0530

    shop app 2 move in project folder and add expo notification api plugin on projects

---
+++ projects/8-Shop-App-Nevigation_5/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import ReduxThunk from 'redux-thunk';
+
+import productsReducer from './app/store/reducers/products';
+import cartReducer from './app/store/reducers/cart';
+import ordersReducer from './app/store/reducers/orders';
+import authReducer from './app/store/reducers/auth';
+import AppNavigator from './app/navigation/AppNavigator';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./app/assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./app/assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
+}
---
+++ projects/8-Shop-App-Nevigation_5/app/store/actions/firebase.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: `https://app-store-project.firebaseio.com`,
+  web_api_key: `AIzaSy-CuTT3ec-Vj2289`
+}
